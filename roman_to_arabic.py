80092e3dfdac79c2d1b4547e2cd90499911620e4
SzybkiRabarbar <maksjoni36@gmail.com>
2022-04-05 17:29:52 +0000

    poprawka

--- roman_to_arabic.py
+++ roman_to_arabic.py
@@ -2,6 +2,7 @@ from classes.Roman_Numerals_Helper import RomanNumerals
 
 def is_roman_valid(roman_number:str):
     if not type(roman_number)==str: return False
+    roman_number = roman_number.upper()
     for rn in roman_number: 
         if not rn in ['I','V','X','L','C','D','M']: return False
     arabic_number = RomanNumerals.from_roman(roman_number)
@@ -13,6 +14,6 @@ if __name__ == '__main__':
     roman_number = input("Wprowadź liczbę rzymską (od 1 do 3999): ")
     is_valid = is_roman_valid(roman_number)
     if is_valid:
-        print(roman_number,'jest równe',is_valid)
+        print(roman_number.upper(),'jest równe',is_valid)
     else:
         print('Źle wprowadzone dane')
